e2146207b69cc12d2f6ecffa308896f95aa82e2c
Evaldas Tyla <evaldas.tyla@oc.eu>
2019-07-23 12:01:05 +0300

    Loading log path from toml

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 extern crate ansi_term;
+extern crate toml;
 
 use ansi_term::Colour;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::thread;
 use std::time::Duration;
+use toml::Value;
 
-const LOG_PATH: &str = "";
+const CONFIG_FILE_NAME: &str = "config.toml";
+const CONFIG_LOG_PATH_PROPERTY: &str = "log_path";
 
 enum LogEvent {
     Error,
@@ -20,7 +23,18 @@ fn main() {
     let mut info_messages: Vec<String> = vec![];
     let mut debug_messages: Vec<String> = vec![];
 
-    let file = File::open(LOG_PATH).expect("Failed opening file");
+    let config = std::fs::read_to_string(CONFIG_FILE_NAME)
+        .expect("Failed loading config file")
+        .parse::<Value>()
+        .expect("Failed loading config values");
+
+    let log_path = config[CONFIG_LOG_PATH_PROPERTY]
+        .as_str()
+        .expect("Failed loading config value log_path");
+
+    let file = File::open(log_path)
+        .expect("Failed opening file");
+        
     let mut reader = BufReader::new(file);
     let mut buffer = String::new();
 
